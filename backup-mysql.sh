6dde02ec04d447934df3c5d63072e3be8d1ea88e
JaeHo Song <thruthesky@gmail.com>
2016-07-18 21:40:40 +0900

    Updating backup-mysql

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 db_user=dev
-db_passwd=***********
+db_passwd=Wc~0453224133
 db_name=dev
 dir_backup=/home/backup/data/database
 error_log=$dir_backup/error.log
@@ -15,15 +15,19 @@ fi
 
 
 
-mysqldump -u$db_user -p$db_passwd --single-transaction --quick --lock-tables=false $db_name > $dir_root_backup/$folder.sql 2>>$error_log
+mysqldump -u$db_user -p$db_passwd --single-transaction --quick --lock-tables=false $db_name > $dir_backup/$folder.sql 2>&1
 
 if [ "$?" = 0 ]; then
-        gzip $dir_root_backup/$folder.sql 2>>$error_log
+	rm -f $dir_backup/$folder.sql.gz
+        gzip $dir_backup/$folder.sql 2>>$error_log
         if [ "$?" = 0 ]; then
-		echo "[$(date)] : success" >>$success_log
+		echo "[$(date)] : success $folder" >>$success_log
 	else
                 echo "[$(date)] : gzip" >>$failure_log
+		rm -f $dir_backup/$folder.sql
         fi
 else
         echo "[$(date)] : mysqldump" >>$failure_log
+	cat $dir_backup/$folder.sql >>$error_log
+	rm -f $dir_backup/$folder.sql
 fi
