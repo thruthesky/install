85b7b123352681c049760c5742bf5da225785449
JaeHo Song <thruthesky@gmail.com>
2016-07-18 21:14:40 +0900

    Updating backup mysql

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -3,23 +3,27 @@
 db_user=dev
 db_passwd=***********
 db_name=dev
-dir_root_backup=/home/backup/data/database
+dir_backup=/home/backup/data/database
+error_log=$dir_backup/error.log
+failure_log=$dir_backup/failure.log
+success_log=$dir_backup/success.log
 
 folder=$(date +%A)
 if [[ $folder == 'Thursday' ]]; then
 folder=${folder}-$(date +%Y-%m)
 fi
 
-#mysqldump -u$db_user -p$db_passwd $db_name | gzip > $dir_root_backup/$folder.gz 2>/dev/null
 
 
-mysqldump -u$db_user -p$db_passwd $db_name > $dir_root_backup/$folder.sql 2>/dev/null
+mysqldump -u$db_user -p$db_passwd --single-transaction --quick --lock-tables=false $db_name > $dir_root_backup/$folder.sql 2>>$error_log
 
 if [ "$?" = 0 ]; then
-        gzip $dir_root_backup/$folder.sql
-        if [ "$?" != 0 ]; then
-                echo "[$(date)] : failed on gzip" >> $dir_root_backup/error.log
+        gzip $dir_root_backup/$folder.sql 2>>$error_log
+        if [ "$?" = 0 ]; then
+		echo "[$(date)] : success" >>$success_log
+	else
+                echo "[$(date)] : gzip" >>$failure_log
         fi
 else
-        echo "[$(date)] : failed on mysqldump" >> $dir_root_backup/error.log
+        echo "[$(date)] : mysqldump" >>$failure_log
 fi
